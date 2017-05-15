8e555cfcd557c0442bbe9d2750505702c32900e8
leagallino <lea.gallino@gmail.com>
2017-05-15 19:22:10 -0300

    agregue mas calles

--- Patrullero_tp/src/domain/Mapa.java
+++ Patrullero_tp/src/domain/Mapa.java
@@ -7,16 +7,16 @@ import edu.uci.ics.jung.graph.DirectedGraph;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 
 public class Mapa {
-	
+
 	DirectedGraph<Esquina, Calle> g;
-	
-	public Mapa(){
+
+	public Mapa() {
 		this.Crear();
 	}
-	
+
 	public void Crear() {
 		g = new DirectedSparseMultigraph<Esquina, Calle>();
-		
+
 		Calle gp68 = new Calle("General Paz", 6800, false, false);
 		Calle gp67 = new Calle("General Paz", 6700, false, false);
 		Calle gp66 = new Calle("General Paz", 6600, false, false);
@@ -32,7 +32,7 @@ public class Mapa {
 		Calle gp56 = new Calle("General Paz", 5600, false, false);
 		Calle gp55 = new Calle("General Paz", 5500, false, false);
 		Calle gp54 = new Calle("General Paz", 5400, false, false);
-		
+
 		Calle SC14 = new Calle("Salvador del Carril", 1400, false, false);
 		Calle SC13 = new Calle("Salvador del Carril", 1300, false, false);
 		Calle SC12 = new Calle("Salvador del Carril", 1200, false, false);
@@ -50,7 +50,7 @@ public class Mapa {
 		Calle OB3 = new Calle("Obispo Boneo", 300, false, false);
 		Calle OB2 = new Calle("Obispo Boneo", 200, false, false);
 		Calle OB1 = new Calle("Obispo Boneo", 100, false, false);
-		
+
 		Calle AB54 = new Calle("Almirante Brown", 5400, false, false);
 		Calle AB55 = new Calle("Almirante Brown", 5500, false, false);
 		Calle AB56 = new Calle("Almirante Brown", 5600, false, false);
@@ -66,7 +66,7 @@ public class Mapa {
 		Calle AB66 = new Calle("Almirante Brown", 6600, false, false);
 		Calle AB67 = new Calle("Almirante Brown", 6700, false, false);
 		Calle AB68 = new Calle("Almirante Brown", 6800, false, false);
-		
+
 		Calle VS54 = new Calle("Velez Sarsfield", 5400, false, false);
 		Calle VS55 = new Calle("Velez Sarsfield", 5500, false, false);
 		Calle VS56 = new Calle("Velez Sarsfield", 5600, false, false);
@@ -81,7 +81,7 @@ public class Mapa {
 		Calle VS65 = new Calle("Velez Sarsfield", 6500, false, false);
 		Calle VS66 = new Calle("Velez Sarsfield", 6600, false, false);
 		Calle VS67 = new Calle("Velez Sarsfield", 6700, false, false);
-		
+
 		Calle Ta54 = new Calle("Tacuari", 5400, false, false);
 		Calle Ta55 = new Calle("Tacuari", 5500, false, false);
 		Calle Ta56 = new Calle("Tacuari", 5600, false, false);
@@ -97,7 +97,7 @@ public class Mapa {
 		Calle Ta66 = new Calle("Tacuari", 6600, false, false);
 		Calle Ta67 = new Calle("Tacuari", 6700, false, false);
 		Calle Ta68 = new Calle("Tacuari", 6800, false, false);
-		
+
 		Calle Pi57 = new Calle("Piedras", 5700, false, false);
 		Calle Pi58 = new Calle("Piedras", 5800, false, false);
 		Calle Pi59 = new Calle("Piedras", 5900, false, false);
@@ -110,52 +110,239 @@ public class Mapa {
 		Calle Pi66 = new Calle("Piedras", 6600, false, false);
 		Calle Pi67 = new Calle("Piedras", 6700, false, false);
 		Calle Pi68 = new Calle("Piedras", 6800, false, false);
+
+		Calle AG59 = new Calle("A Godoy", 5900, false, false);
+		Calle AG60 = new Calle("A Godoy", 6000, false, false);
+		Calle AG61 = new Calle("A Godoy", 6100, false, false);
+		Calle AG62 = new Calle("A Godoy", 6200, false, false);
+		Calle AG63 = new Calle("A Godoy", 6300, false, false);
+		Calle AG64 = new Calle("A Godoy", 6400, false, false);
+		Calle AG65 = new Calle("A Godoy", 6500, false, false);
+		Calle AG66 = new Calle("A Godoy", 6600, false, false);
+		Calle AG67 = new Calle("A Godoy", 6700, false, false);
+		Calle AG68 = new Calle("A Godoy", 6800, false, false);
+
+		Calle Cu60 = new Calle("Cullen", 6000, false, false);
+		Calle Cu61 = new Calle("Cullen", 6100, false, false);
+		Calle Cu62 = new Calle("Cullen", 6200, false, false);
+		Calle Cu63 = new Calle("Cullen", 6300, false, false);
+		Calle Cu64 = new Calle("Cullen", 6400, false, false);
+		Calle Cu65 = new Calle("Cullen", 6500, false, false);
+		Calle Cu66 = new Calle("Cullen", 6600, false, false);
+		Calle Cu67 = new Calle("Cullen", 6700, false, false);
+		Calle Cu68 = new Calle("Cullen", 6800, false, false);
+
+		Calle Ec62 = new Calle("Echague", 6200, false, false);
+		Calle Ec63 = new Calle("Echague", 6300, false, false);
+		Calle Ec64 = new Calle("Echague", 6400, false, false);
+		Calle Ec65 = new Calle("Echague", 6500, false, false);
+		Calle Ec66 = new Calle("Echague", 6600, false, false);
+		Calle Ec67 = new Calle("Echague", 6700, false, false);
+		Calle Ec68 = new Calle("Echague", 6800, false, false);
+
+		Calle To64 = new Calle("Talcahuano", 6400, false, false);
+		Calle To65 = new Calle("Talcahuano", 6500, false, false);
+		Calle To66 = new Calle("Talcahuano", 6600, false, false);
+		Calle To67 = new Calle("Talcahuano", 6700, false, false);
+		Calle To68 = new Calle("Talcahuano", 6800, false, false);
+
+		Calle De65 = new Calle("Defensa", 6500, false, false);
+		Calle De66 = new Calle("Defensa", 6600, false, false);
+		Calle De67 = new Calle("Defensa", 6700, false, false);
+		Calle De68 = new Calle("Defensa", 6800, false, false);
+
+		Calle Ri66 = new Calle("Riobamba", 6600, false, false);
+		Calle Ri67 = new Calle("Riobamba", 6700, false, false);
+		Calle Ri68 = new Calle("Riobamba", 6800, false, false);
+
+		Calle PP54 = new Calle("Pasaje Pizzorino", 5400, false, false);
+		Calle PP55 = new Calle("Pasaje Pizzorino", 5500, false, false);
+		Calle PP56 = new Calle("Pasaje Pizzorino", 5600, false, false);
+		Calle PP57 = new Calle("Pasaje Pizzorino", 5700, false, false);
 		
-				
+		Calle CP61 = new Calle("Calle Pizzorino", 6100, false, false);
+		
+		Calle PA58 = new Calle("Pasaje Arzamendia", 5800, false, false);
+		Calle PA59 = new Calle("Pasaje Arzamendia", 5900, false, false);
+
+		Calle Do54 = new Calle("Dorrego", 5400, false, false);
+		Calle Do55 = new Calle("Dorrego", 5500, false, false);
+		Calle Do56 = new Calle("Dorrego", 5600, false, false);
+		Calle Do57 = new Calle("Dorrego", 5700, false, false);
+		Calle Do58 = new Calle("Dorrego", 5800, false, false);
+		Calle Do59 = new Calle("Dorrego", 5900, false, false);
+		Calle Do60 = new Calle("Dorrego", 6000, false, false);
+		Calle Do61 = new Calle("Dorrego", 6100, false, false);
+		Calle Do62 = new Calle("Dorrego", 6200, false, false);
+		Calle Do63 = new Calle("Dorrego", 6300, false, false);
+
+		Calle Av54 = new Calle("Avellaneda", 5400, false, false);
+		Calle Av55 = new Calle("Avellaneda", 5500, false, false);
+		Calle Av56 = new Calle("Avellaneda", 5600, false, false);
+		Calle Av57 = new Calle("Avellaneda", 5700, false, false);
+		Calle Av58 = new Calle("Avellaneda", 5800, false, false);
+		Calle Av59 = new Calle("Avellaneda", 5900, false, false);
+		Calle Av60 = new Calle("Avellaneda", 6000, false, false);
+
+		Calle Gu54 = new Calle("Guemes", 5400, false, false);
+		Calle Gu55 = new Calle("Guemes", 5500, false, false);
+		Calle Gu56 = new Calle("Guemes", 5600, false, false);
+		Calle Gu57 = new Calle("Guemes", 5700, false, false);
+
+		Calle La54 = new Calle("Lavalle", 5400, false, false);
+		Calle La55 = new Calle("Lavalle", 5500, false, false);
+		
+		Calle EZ1 = new Calle("Estanislao Zeballos", 100, false, false);
+		Calle EZ2 = new Calle("Estanislao Zeballos", 200, false, false);
+		Calle EZ3 = new Calle("Estanislao Zeballos", 300, false, false);
+		Calle EZ4 = new Calle("Estanislao Zeballos", 400, false, false);
+		Calle EZ5 = new Calle("Estanislao Zeballos", 500, false, false);
+		Calle EZ6 = new Calle("Estanislao Zeballos", 600, false, false);
+		Calle EZ7 = new Calle("Estanislao Zeballos", 700, false, false);
+		Calle EZ8 = new Calle("Estanislao Zeballos", 800, false, false);
+		Calle EZ9 = new Calle("Estanislao Zeballos", 900, false, false);
+		Calle EZ10 = new Calle("Estanislao Zeballos", 1000, false, false);
+		
+		Calle JC1 = new Calle("Juan Castelli", 100, false, false);
+		Calle JC2 = new Calle("Juan Castelli", 200, false, false);
+		Calle JC3 = new Calle("Juan Castelli", 300, false, false);
+		Calle JC4 = new Calle("Juan Castelli", 400, false, false);
+		Calle JC5 = new Calle("Juan Castelli", 500, false, false);
+		Calle JC6 = new Calle("Juan Castelli", 600, false, false);
+		Calle JC7 = new Calle("Juan Castelli", 700, false, false);
+		Calle JC8 = new Calle("Juan Castelli", 800, false, false);
+		Calle JC9 = new Calle("Juan Castelli", 900, false, false);
+		Calle JC10 = new Calle("Juan Castelli", 1000, false, false);
+		
+		Calle CH2 = new Calle("Calle Hernandarias", 200, false, false);
+		Calle CH3 = new Calle("Calle Hernandarias", 300, false, false);
+		Calle CH4 = new Calle("Calle Hernandarias", 400, false, false);
+		Calle CH5 = new Calle("Calle Hernandarias", 500, false, false);
+		Calle CH6 = new Calle("Calle Hernandarias", 600, false, false);
+		Calle CH7 = new Calle("Calle Hernandarias", 700, false, false);
+		Calle CH8 = new Calle("Calle Hernandarias", 800, false, false);
+		Calle CH9 = new Calle("Calle Hernandarias", 900, false, false);
+		Calle CH10 = new Calle("Calle Hernandarias", 1000, false, false);
+
+		Calle JPL3 = new Calle("JP Lopez", 300, false, false);
+		Calle JPL4 = new Calle("JP Lopez", 400, false, false);
+		Calle JPL5 = new Calle("JP Lopez", 500, false, false);
+		Calle JPL6 = new Calle("JP Lopez", 600, false, false);
+		Calle JPL7 = new Calle("JP Lopez", 700, false, false);
+		Calle JPL8 = new Calle("JP Lopez", 800, false, false);
+		Calle JPL9 = new Calle("JP Lopez", 900, false, false);
+		Calle JPL10 = new Calle("JP Lopez", 1000, false, false);
+
+		Calle AC3 = new Calle("Angel Cassanello", 300, false, false);
+		Calle AC4 = new Calle("Angel Cassanello", 400, false, false);
+		Calle AC5 = new Calle("Angel Cassanello", 500, false, false);
+		Calle AC6 = new Calle("Angel Cassanello", 600, false, false);
+		Calle AC7 = new Calle("Angel Cassanello", 700, false, false);
+		Calle AC8 = new Calle("Angel Cassanello", 800, false, false);
+		Calle AC9 = new Calle("Angel Cassanello", 900, false, false);
+		Calle AC10 = new Calle("Angel Cassanello", 1000, false, false);
+		
+		Calle RA4 = new Calle("Ricardo Aldao", 400, false, false);
+		Calle RA5 = new Calle("Ricardo Aldao", 500, false, false);
+		Calle RA6 = new Calle("Ricardo Aldao", 600, false, false);
+		Calle RA7 = new Calle("Ricardo Aldao", 700, false, false);
+		Calle RA8 = new Calle("Ricardo Aldao", 800, false, false);
+		Calle RA9 = new Calle("Ricardo Aldao", 900, false, false);
+		Calle RA10 = new Calle("Ricardo Aldao", 1000, false, false);
+		Calle RA11 = new Calle("Ricardo Aldao", 1100, false, false);
+		
+
+		Calle PV4 = new Calle("Pedro de Vega", 400, false, false);
+		Calle PV5 = new Calle("Pedro de Vega", 500, false, false);
+		Calle PV6 = new Calle("Pedro de Vega", 600, false, false);
+		Calle PV7 = new Calle("Pedro de Vega", 700, false, false);
+		Calle PV8 = new Calle("Pedro de Vega", 800, false, false);
+		Calle PV9 = new Calle("Pedro de Vega", 900, false, false);
+		Calle PV10 = new Calle("Pedro de Vega", 1000, false, false);
+		Calle PV11 = new Calle("Pedro de Vega", 1100, false, false);
+		
+		Calle PI8 = new Calle("Pasaje Iturri", 800, false, false);
+		Calle PI9 = new Calle("Pasaje Iturri", 900, false, false);
+		
+		Calle Go7 = new Calle("Gorostiaga", 700, false, false);
+		Calle Go8 = new Calle("Gorostiaga", 800, false, false);
+		Calle Go9 = new Calle("Gorostiaga", 900, false, false);
+
+		Calle MS7 = new Calle("Marco Sastre", 700, false, false);
+		Calle MS8 = new Calle("Marco Sastre", 800, false, false);
+		Calle MS9 = new Calle("Marco Sastre", 900, false, false);
+		
+		Calle DH7 = new Calle("D Huergo", 700, false, false);
+		Calle DH8 = new Calle("D Huergo", 800, false, false);
+		Calle DH9 = new Calle("D Huergo", 900, false, false);
+		Calle DH10 = new Calle("D Huergo", 1000, false, false);
+		Calle DH11 = new Calle("D Huergo", 1100, false, false);
+		Calle DH12 = new Calle("D Huergo", 1200, false, false);
+		Calle DH13 = new Calle("D Huergo", 1300, false, false);
+		
+		Calle RG8 = new Calle("Ruperto Godoy", 800, false, false);
+		Calle RG9 = new Calle("Ruperto Godoy", 900, false, false);
+		Calle RG10 = new Calle("Ruperto Godoy", 1000, false, false);
+		Calle RG11 = new Calle("Ruperto Godoy", 1100, false, false);
+		Calle RG12 = new Calle("Ruperto Godoy", 1200, false, false);
+		Calle RG13 = new Calle("Ruperto Godoy", 1300, false, false);
+		
+		Calle SD8 = new Calle("Santiago Derqui", 800, false, false);
+		Calle SD9 = new Calle("Santiago Derqui", 900, false, false);
+		Calle SD10 = new Calle("Santiago Derqui", 1000, false, false);
+		Calle SD11 = new Calle("Santiago Derqui", 1100, false, false);
+		Calle SD12 = new Calle("Santiago Derqui", 1200, false, false);
+		Calle SD13 = new Calle("Santiago Derqui", 1300, false, false);
+		
+		Calle JMZ9 = new Calle("Jose Maria Zuviria", 900, false, false);
+		Calle JMZ10 = new Calle("Jose Maria Zuviria", 1000, false, false);
+		Calle JMZ11 = new Calle("Jose Maria Zuviria", 1100, false, false);
+		Calle JMZ12 = new Calle("Jose Maria Zuviria", 1200, false, false);
+		Calle JMZ13 = new Calle("Jose Maria Zuviria", 1300, false, false);
+		Calle JMZ14 = new Calle("Jose Maria Zuviria", 1400, false, false);
+		
 		Esquina e1 = new Esquina("e1");
 		Esquina e2 = new Esquina("e2");
 		Esquina e3 = new Esquina("e3");
-		
+
 		g.addVertex(e1);
 		g.addVertex(e2);
 		g.addVertex(e3);
-		
-		
-		
+
 		g.addEdge(c1, e1, e2);
 		g.addEdge(c2, e2, e3);
 		g.addEdge(c3, e3, e1);
-		
-		System.out.println("Sucesores"+g.getSuccessors(e2));
-		
-		System.out.println("Grafo"+g.toString());
+
+		System.out.println("Sucesores" + g.getSuccessors(e2));
+
+		System.out.println("Grafo" + g.toString());
 	}
-	
-	public List<Calle> getCallesCortadas(){
+
+	public List<Calle> getCallesCortadas() {
 		List<Calle> callesCortadas = new ArrayList<Calle>();
-		
+
 		return callesCortadas;
 	}
-	
-	public List<Calle> getCallesCongestionadas(){
+
+	public List<Calle> getCallesCongestionadas() {
 		List<Calle> callesCongestionadas = new ArrayList<Calle>();
-		
+
 		return callesCongestionadas;
 	}
 
-	public Esquina getAlerta(){
+	public Esquina getAlerta() {
 		Esquina alerta = null;
-		
-		return alerta;		
+
+		return alerta;
 	}
-	
-	public Esquina getPosicionAgente(){
+
+	public Esquina getPosicionAgente() {
 		Esquina posAgente = null;
-		
-		return posAgente;		
+
+		return posAgente;
 	}
-	
-	public List<Esquina> getAdyacentes(Esquina esquina){
+
+	public List<Esquina> getAdyacentes(Esquina esquina) {
 		return (List<Esquina>) g.getSuccessors(esquina);
 	}
 }
